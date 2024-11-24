55db3170b0bf8a8f59e98b6f95a7ddc7f520dce3
redwanfarooq <redwan.farooq@ndcn.ox.ac.uk>
2024-11-24 19:51:06 +0000

    Added scVI integration for multimodal data

--- resources/scripts/integration_functions.R
+++ resources/scripts/integration_functions.R
@@ -4,7 +4,7 @@
 
 
 # Explicitly define set if null operator
-`%||%` <- SeuratObject::`%||%`
+`%||%` <- rlang::`%||%`
 
 
 # Customised general wrapper functions for integration methods
@@ -103,6 +103,77 @@ FastMNNIntegration <- function(object,
   return(output.list)
 }
 attr(x = FastMNNIntegration, which = "Seurat.method") <- "integration"
+
+
+scVIIntegration <- function(object,
+                            assay = NULL,
+                            layers = NULL,
+                            orig = NULL,
+                            new.reduction = "integrated.dr",
+                            features = NULL,
+                            normalization.method = c("LogNormalize", "SCT"),
+                            dims = 1:30,
+                            scale.layer = "scale.data",
+                            max_epochs = NULL,
+                            verbose = TRUE,
+                            ...) {
+  features <- features %||% Seurat::SelectIntegrationFeatures5(object = object)
+  assay <- assay %||% SeuratObject::DefaultAssay(object = object)
+  layers <- layers %||% SeuratObject::Layers(object = object, search = "data")
+  groups <- Seurat:::CreateIntegrationGroups(object, layers = layers, scale.layer = scale.layer)
+
+  if (verbose) message("Loading Python libraries")
+  ad <- reticulate::import("anndata", convert = FALSE)
+  scipy <- reticulate::import("scipy", convert = FALSE)
+  scvi <- reticulate::import("scvi", convert = FALSE)
+
+  # infer modality from assay name to select correct scVI model
+  model <- dplyr::case_when(
+    grepl(pattern = "rna|sct|gene", x = assay, ignore.case = TRUE) ~ "SCVI",
+    grepl(pattern = "atac|peak|chrom", x = assay, ignore.case = TRUE) ~ "PEAKVI",
+    grepl(pattern = "adt|cite|prot", x = assay, ignore.case = TRUE) ~ "TOTALVI",
+    TRUE ~ NA
+  )
+  if (is.na(model)) stop("Unable to infer modality from assay name: ", assay)
+
+  # extract counts, integration groups and feature metadata and create AnnData object
+  if (is(object, "Assay5")) object <- SeuratObject::JoinLayers(object = object, layers = "counts")
+  args.adata <- list(obs = groups)
+  args.setup <- list(batch_key = "group")
+  if (model == "TOTALVI") {
+    # create a dummy gene count matrix
+    args.adata$X <- scipy$sparse$csr_matrix(SeuratObject::SparseEmptyMatrix(nrow = ncol(object), ncol = 1, rownames = colnames(object), colnames = "XXX"))
+    args.adata$var <- NULL
+    # get protein count matrix
+    args.adata$obsm <- list(protein = as.data.frame(Matrix::t(LayerData(object = object, layer = "counts")[features, ])))
+    args.setup$protein_expression_obsm_key <- "protein"
+  } else {
+    # get gene or peak count matrix
+    args.adata$X <- scipy$sparse$csr_matrix(Matrix::t(LayerData(object = object, layer = "counts")[features, ]))
+    args.adata$var <- object[[]][features, ]
+  }
+  adata <- do.call(ad$AnnData, args.adata)
+  args.setup$adata <- adata
+
+  # set up and train the model
+  if (verbose) message("Running ", model)
+  do.call(scvi$model[[model]]$setup_anndata, args.setup)
+  vae <- scvi$model[[model]](adata = adata, ...)
+  vae$train(max_epochs = max_epochs)
+  # extract the latent embedding
+  latent <- vae$get_latent_representation() |> as.matrix()
+  rownames(latent) <- reticulate::py_to_r(adata$obs$index$values)
+  merged <- SeuratObject::CreateDimReducObject(
+    embeddings = latent,
+    assay = assay,
+    key = paste0(gsub(pattern = "[^[:alpha:]]", replacement = "", x = new.reduction), "_")
+  ) %>%
+    suppressWarnings()
+  output.list <- list(merged)
+  names(output.list) <- c(new.reduction)
+  return(output.list)
+}
+attr(x = scVIIntegration, which = "Seurat.method") <- "integration"
 
 
 # Customised ChromatinAssay-specific wrapper functions for integration methods
